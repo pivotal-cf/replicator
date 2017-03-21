531dd710e85834a44da0f95b83ac8125fba5a104
Kevin Kelani <kkelani@pivotal.io>
2017-03-21 14:52:17 -0700

    use constants in metadata replication
    [#139590621]

--- replicator/tile_replicator.go
+++ replicator/tile_replicator.go
@@ -13,9 +13,19 @@ import (
 
 var metadataRegexp = regexp.MustCompile(`metadata\/.*\.yml$`)
 
-var ymlRegexp = regexp.MustCompile(`.*\.yml$`)
-var defaultIsoSegNamePrefix = "p-isolation-segment"
-var defaultIsoSegCellNamePrefix = ".isolated_diego_cell"
+const (
+	defaultIsoSegName      = "p-isolation-segment"
+	defaultRouterJobType   = "isolated_router"
+	defaultRouterStaticIPs = ".isolated_router.static_ips"
+
+	defaultCellJobType                           = "isolated_diego_cell"
+	defaultCellDockerFormTypeRef                 = ".isolated_diego_cell.insecure_docker_registry_list"
+	defaultCellPlacementTagFormTypeRef           = ".isolated_diego_cell.placement_tag"
+	defaultCellGardenNetworkPoolFormTypeRef      = ".isolated_diego_cell.garden_network_pool"
+	defaultCellGardenNetworkMTUFormTypeRef       = ".isolated_diego_cell.garden_network_mtu"
+	defaultCellExecutorMemoryCapacityFormTypeRef = ".isolated_diego_cell.executor_memory_capacity"
+	defaultCellExecutorDiskCapacityFormTypeRef   = ".isolated_diego_cell.executor_disk_capacity"
+)
 
 type TileReplicator struct{}
 
@@ -70,24 +80,24 @@ func (t TileReplicator) Replicate(config ApplicationConfig) error {
 			var metadata Metadata
 			yaml.Unmarshal(contents, &metadata)
 
-			metadata.Name = defaultIsoSegNamePrefix + "-" + config.Name
+			metadata.Name = defaultIsoSegName + "-" + config.Name
 
 			t.findAndReplaceJobTypeName(metadata.JobTypes, "isolated_router", fmt.Sprintf("isolated_router_%s", config.Name))
-			t.findAndReplaceFormTypeRef(metadata.FormTypes, ".isolated_router.static_ips",
+			t.findAndReplaceFormTypeRef(metadata.FormTypes, defaultRouterStaticIPs,
 				fmt.Sprintf(".isolated_router_%s.static_ips", config.Name))
 
-			t.findAndReplaceJobTypeName(metadata.JobTypes, "isolated_diego_cell", fmt.Sprintf("isolated_diego_cell_%s", config.Name))
-			t.findAndReplaceFormTypeRef(metadata.FormTypes, ".isolated_diego_cell.insecure_docker_registry_list",
+			t.findAndReplaceJobTypeName(metadata.JobTypes, defaultCellJobType, fmt.Sprintf("%s_%s", defaultCellJobType, config.Name))
+			t.findAndReplaceFormTypeRef(metadata.FormTypes, defaultCellDockerFormTypeRef,
 				fmt.Sprintf(".isolated_diego_cell_%s.insecure_docker_registry_list", config.Name))
-			t.findAndReplaceFormTypeRef(metadata.FormTypes, ".isolated_diego_cell.placement_tag",
+			t.findAndReplaceFormTypeRef(metadata.FormTypes, defaultCellPlacementTagFormTypeRef,
 				fmt.Sprintf(".isolated_diego_cell_%s.placement_tag", config.Name))
-			t.findAndReplaceFormTypeRef(metadata.FormTypes, ".isolated_diego_cell.garden_network_pool",
+			t.findAndReplaceFormTypeRef(metadata.FormTypes, defaultCellGardenNetworkPoolFormTypeRef,
 				fmt.Sprintf(".isolated_diego_cell_%s.garden_network_pool", config.Name))
-			t.findAndReplaceFormTypeRef(metadata.FormTypes, ".isolated_diego_cell.garden_network_mtu",
+			t.findAndReplaceFormTypeRef(metadata.FormTypes, defaultCellGardenNetworkMTUFormTypeRef,
 				fmt.Sprintf(".isolated_diego_cell_%s.garden_network_mtu", config.Name))
-			t.findAndReplaceFormTypeRef(metadata.FormTypes, ".isolated_diego_cell.executor_memory_capacity",
+			t.findAndReplaceFormTypeRef(metadata.FormTypes, defaultCellExecutorMemoryCapacityFormTypeRef,
 				fmt.Sprintf(".isolated_diego_cell_%s.executor_memory_capacity", config.Name))
-			t.findAndReplaceFormTypeRef(metadata.FormTypes, ".isolated_diego_cell.executor_disk_capacity",
+			t.findAndReplaceFormTypeRef(metadata.FormTypes, defaultCellExecutorDiskCapacityFormTypeRef,
 				fmt.Sprintf(".isolated_diego_cell_%s.executor_disk_capacity", config.Name))
 
 			contentsYaml, err := yaml.Marshal(metadata)
