53fbc90360cea05af033b3adb48c3900c53b0063
Josh Zarrabi <jzarrabi@pivotal.io>
2018-03-07 10:59:54 -0800

    Consolidate the fixtures into a single top-level dir
    [#155641859]
    
    Signed-off-by: Dave Walter <dwalter@pivotal.io>

--- acceptance/replicate_tile_test.go
+++ acceptance/replicate_tile_test.go
@@ -13,7 +13,7 @@ import (
 var _ = Describe("replicator", func() {
 	Context("when replicating the isolation segment tile", func() {
 		It("writes a file without erroring", func() {
-			pathToTile := filepath.Join("fixtures", "ist.pivotal")
+			pathToTile := filepath.Join("..", "fixtures", "ist.pivotal")
 			pathToOutputTile := filepath.Join(os.TempDir(), "ist-duplicated.pivotal")
 
 			command := exec.Command(pathToMain,
@@ -28,9 +28,10 @@ var _ = Describe("replicator", func() {
 			Expect(pathToOutputTile).To(BeAnExistingFile())
 		})
 	})
-	Context("when replicating the windows runtime tile", func() {
+
+	Context("when replicating the windows 2012 runtime tile", func() {
 		It("writes a file without erroring", func() {
-			pathToTile := filepath.Join("fixtures", "wrt.pivotal")
+			pathToTile := filepath.Join("..", "fixtures", "wrt.pivotal")
 			pathToOutputTile := filepath.Join(os.TempDir(), "wrt-duplicated.pivotal")
 
 			command := exec.Command(pathToMain,
--- replicator/tile_replicator_test.go
+++ replicator/tile_replicator_test.go
@@ -24,66 +24,17 @@ var _ = Describe("tile replicator", func() {
 	)
 
 	Describe("Replicate", func() {
-		Context("when replicating the windows runtime tile", func() {
-			BeforeEach(func() {
-				pathToTile = filepath.Join("fixtures", "wrt.pivotal")
-
-				tempDir, err := ioutil.TempDir("", "")
-				Expect(err).NotTo(HaveOccurred())
-				pathToOutputTile = filepath.Join(tempDir, "replicated-tile.pivotal")
-
-				expectedMetadataFile := filepath.Join("fixtures", "expected-wrt-metadata.yml")
-
-				contents, err := ioutil.ReadFile(expectedMetadataFile)
-				Expect(err).NotTo(HaveOccurred())
-				expectedMetadata = string(contents)
-
-				tileReplicator = replicator.NewTileReplicator()
-			})
-
-			It("creates a duplicate tile with modified metadata", func() {
-				err := tileReplicator.Replicate(replicator.ApplicationConfig{
-					Path:   pathToTile,
-					Output: pathToOutputTile,
-					Name:   "Azure Sea",
-				})
-				Expect(err).NotTo(HaveOccurred())
-
-				zr, err := zip.OpenReader(pathToOutputTile)
-				Expect(err).NotTo(HaveOccurred())
-
-				defer zr.Close()
-
-				var metadata *zip.File
-				for _, file := range zr.File {
-					if file.Name == "metadata/p-windows-runtime.yml" {
-						metadata = file
-						break
-					}
-				}
-				Expect(metadata).NotTo(BeNil())
-
-				f, err := metadata.Open()
-				Expect(err).NotTo(HaveOccurred())
-
-				contents, err := ioutil.ReadAll(f)
-				Expect(err).NotTo(HaveOccurred())
-
-				Expect(string(contents)).To(gomegamatchers.MatchYAML(expectedMetadata))
-			})
-		})
-
 		Context("when replicating the isolation segment tile", func() {
 			BeforeEach(func() {
-				pathToTile = filepath.Join("fixtures", "ist.pivotal")
-				pathToAlreadyDuplicatedTile = filepath.Join("fixtures", "ist-duplicated.pivotal")
-				pathToInvalidYamlMetadata = filepath.Join("fixtures", "invalid-metadata.pivotal")
+				pathToTile = filepath.Join("..", "fixtures", "ist.pivotal")
+				pathToAlreadyDuplicatedTile = filepath.Join("..", "fixtures", "ist-duplicated.pivotal")
+				pathToInvalidYamlMetadata = filepath.Join("..", "fixtures", "invalid-metadata.pivotal")
 
 				tempDir, err := ioutil.TempDir("", "")
 				Expect(err).NotTo(HaveOccurred())
 				pathToOutputTile = filepath.Join(tempDir, "replicated-tile.pivotal")
 
-				expectedMetadataFile := filepath.Join("fixtures", "expected-ist-metadata.yml")
+				expectedMetadataFile := filepath.Join("..", "fixtures", "expected-ist-metadata.yml")
 
 				contents, err := ioutil.ReadFile(expectedMetadataFile)
 				Expect(err).NotTo(HaveOccurred())
@@ -125,8 +76,8 @@ var _ = Describe("tile replicator", func() {
 
 			Context("when a property does not exist in the tile metadata", func() {
 				It("does not fail to replicate the tile", func() {
-					pathToTile = filepath.Join("fixtures", "some-tile-with-missing-property.pivotal")
-					expectedMetadataFile := filepath.Join("fixtures", "expected-metadata-with-missing-property.yml")
+					pathToTile = filepath.Join("..", "fixtures", "some-tile-with-missing-property.pivotal")
+					expectedMetadataFile := filepath.Join("..", "fixtures", "expected-metadata-with-missing-property.yml")
 					contents, err := ioutil.ReadFile(expectedMetadataFile)
 					Expect(err).NotTo(HaveOccurred())
 					expectedMetadata = string(contents)
@@ -213,6 +164,55 @@ var _ = Describe("tile replicator", func() {
 						Expect(err).To(MatchError("could not create destination tile"))
 					})
 				})
+			})
+		})
+
+		Context("when replicating the windows 2012 runtime tile", func() {
+			BeforeEach(func() {
+				pathToTile = filepath.Join("..", "fixtures", "wrt.pivotal")
+
+				tempDir, err := ioutil.TempDir("", "")
+				Expect(err).NotTo(HaveOccurred())
+				pathToOutputTile = filepath.Join(tempDir, "replicated-tile.pivotal")
+
+				expectedMetadataFile := filepath.Join("..", "fixtures", "expected-wrt-metadata.yml")
+
+				contents, err := ioutil.ReadFile(expectedMetadataFile)
+				Expect(err).NotTo(HaveOccurred())
+				expectedMetadata = string(contents)
+
+				tileReplicator = replicator.NewTileReplicator()
+			})
+
+			It("creates a duplicate tile with modified metadata", func() {
+				err := tileReplicator.Replicate(replicator.ApplicationConfig{
+					Path:   pathToTile,
+					Output: pathToOutputTile,
+					Name:   "Azure Sea",
+				})
+				Expect(err).NotTo(HaveOccurred())
+
+				zr, err := zip.OpenReader(pathToOutputTile)
+				Expect(err).NotTo(HaveOccurred())
+
+				defer zr.Close()
+
+				var metadata *zip.File
+				for _, file := range zr.File {
+					if file.Name == "metadata/p-windows-runtime.yml" {
+						metadata = file
+						break
+					}
+				}
+				Expect(metadata).NotTo(BeNil())
+
+				f, err := metadata.Open()
+				Expect(err).NotTo(HaveOccurred())
+
+				contents, err := ioutil.ReadAll(f)
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(string(contents)).To(gomegamatchers.MatchYAML(expectedMetadata))
 			})
 		})
 	})
